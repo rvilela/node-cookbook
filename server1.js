2b739231eaa9edecd819c51f2f1217b408583c8a
Ricardo Vilela <rvilela52@gmail.com>
2013-01-16 15:27:35 -0200

    Exercicios Serving static files

--- server1.js
+++ server1.js
@@ -1,27 +1,29 @@
 var http = require('http');
-var url = require('url');
-var pages = [
-    {id: '1', route: '', output: 'Woohoo!'},
-    {id: '2', route: 'about', output: 'A simple routing with Node example'},
-	{id: '3', route: 'another page', output: function () {return
-		'Here\'s ' + this.route; }},
-		];
+var path = require('path');
+var fs = require('fs');
+
+var mimeTypes = {
+  '.js' : 'text/javascript',
+  '.html': 'text/html',
+  '.css' : 'text/css'
+};
 
 var processRequest = function(request, response) {
-	var id = url.parse(decodeURI(request.url), true).query.id;
-	if (id) {
-		pages.forEach(function (page) {
-			if (page.id === id) {
-				response.writeHead(200, {'Content-Type': 'text/html'});
-				response.end(typeof page.output === 'function'
-					? page.output() : page.output);
-			}
-		});
-	}
-	if (!response.finished) {
-		response.writeHead(404);
-		response.end('Page Not Found');
-	}
+	  var lookup = path.basename(decodeURI(request.url)) || 'index.html',
+	    f = 'content/' + lookup;
+	  fs.exists(f, function (exists) { //path.exists for Node 0.6 and below
+	    if (exists) {
+	      fs.readFile(f, function (err, data) {
+	        if (err) {response.writeHead(500); response.end('Server Error!'); return; }
+	        var headers = {'Content-type': mimeTypes[path.extname(lookup)]};
+	        response.writeHead(200, headers);
+	        response.end(data);
+	      });
+	      return;
+	    }
+	    response.writeHead(404); //no such file found!
+	    response.end('Page Not Found!');
+	  });
 };
 
 http.createServer(processRequest).listen(80);
